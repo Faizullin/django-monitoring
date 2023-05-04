42e783f9004534c8268c1a7efef88bb1e1b78ca9
Faizullin <osmanfajzulla@gmail.com>
2023-05-04 10:32:11 +0600

    save

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,7 +1,7 @@
 def default_context(request = None):
     return {
         'servers': {
-            'server0' : 'http://localhost:8000',
+            'server0' : 'https://rfid-kassa.com',
             'server1' : 'https://www.sys-monitoring.kz',
         },
     }
